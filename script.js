96f08965a4e3c9ffec15cc2ad473f33e782ae495
Ahmad-U19 <ahmedoppal2592@gmail.com>
2024-11-04 21:40:24 +0500

    Final Files

---
+++ script.js
@@ -0,0 +1,12 @@
+var sc="Ahmad Hassaan";
+var ag=19;
+
+document.write(sc);
+document.write("<br>");
+document.write(ag);
+
+
+document.write("<br>My Name is Ahmad<br>");
+//My First Comment In Java Script
+//document.write("Get Lost!!!");
+document.write("<i><b>Hello World !!</b></i>");
